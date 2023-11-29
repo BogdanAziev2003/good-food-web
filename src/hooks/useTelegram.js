b00e059a86ee1c1103e3126d352bf7c9f74ba478
Zaur Taysaev <z.taysaev2003@mail.ru>
2023-11-29 18:01:42 +0300

    telegram dark mode to default

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -10,7 +10,7 @@ export function useTelegram() {
 
   const tg = window.Telegram.WebApp
   tg.expand()
-  tg.colorScheme = 'night'
+  tg.setTheme('night')
   tg.MainButton.textColor = '#333'
   tg.MainButton.color = '#ffdf2c'
 
