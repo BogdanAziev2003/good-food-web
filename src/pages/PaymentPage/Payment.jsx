2ade26dfc72bb495acebb8ea89953f27ac731c16
Zaur Taysaev <z.taysaev2003@mail.ru>
2023-11-30 12:09:58 +0300

    address null test

--- src/pages/PaymentPage/Payment.jsx
+++ src/pages/PaymentPage/Payment.jsx
@@ -123,7 +123,7 @@ const Payment = React.memo(() => {
   useEffect(() => {
     tg.onEvent('mainButtonClicked', () => {
       if (store.phone === null) setPhoneError(true)
-      if (store.address === null) {
+      if (store.address === null && store.deliveryType === 'delivery') {
         setAdressError(true)
       }
     })
