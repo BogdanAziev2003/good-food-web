beeb2513c48c777c199e7499209ffe8248ef3884
Zaur Taysaev <z.taysaev2003@mail.ru>
2023-11-30 18:04:49 +0300

    bug fix

--- src/components/Payment/Delivery.jsx
+++ src/components/Payment/Delivery.jsx
@@ -65,9 +65,7 @@ const Delivery = ({ addressError, setAddressError }) => {
       )}
       <div className="address_error">{errorMessage ? errorMessage : <></>}</div>
       <div className="address_error">
-        {addressError && (
-          <p className="address_error">Введите адресс доставки</p>
-        )}
+        {addressError && <p className="address_error">Введите адресс</p>}
       </div>
     </div>
   )
