e1d1cd6c91425f5ed1e977ae68c60961eba3790f
Zaur Taysaev <z.taysaev2003@mail.ru>
2023-12-04 13:30:07 +0300

    Add Component ItemModifiers

---
+++ src/components/BasicModalComponent/ItemModifiers/ItemModifiers.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import {
+  addSupplement,
+  removeSupplement,
+} from '../../../store/features/itemsSlice'
+import ItemDrink from './ItemDrink'
+
+const ItemModifiers = ({ el, item, curItem, itemInCard, setCurValue }) => {
+  const dispatch = useDispatch()
+
+  const handleAddSupplement = (el) => {
+    item = curItem
+    dispatch(addSupplement({ el, item }))
+    item = itemInCard.find((el) => el.idInCard === curItem.idInCard)
+    setCurValue(item)
+  }
+
+  const handleRemoveSupplement = (el) => {
+    item = curItem
+    dispatch(removeSupplement({ el, item }))
+  }
+
+  const handleOptionChange = (drink) => {
+    if (drink.amount === 1) {
+      return
+    }
+    handleAddSupplement(drink)
+  }
+  return (
+    <div className="modal__item mod">
+      <div className="mod__name">
+        <p>{el.title}</p>
+      </div>
+
+      {curItem.category !== 'Напитки' ? (
+        el.amount !== 0 ? (
+          <div className="mod__add mod__add_active">
+            <button
+              className="mod__add-btn"
+              onClick={() => {
+                handleAddSupplement(el)
+              }}
+            >
+              +
+            </button>
+            <p className="mod__el__amount">{el.amount}</p>
+            <button
+              className="mod__add-btn mod__add-btn-minus"
+              onClick={() => {
+                handleRemoveSupplement(el)
+              }}
+            >
+              -
+            </button>
+          </div>
+        ) : (
+          <div
+            className="mod__add"
+            onClick={() => {
+              handleAddSupplement(el)
+            }}
+          >
+            <div className="mod__add-plus">
+              <p>+</p>
+            </div>
+
+            <div className="mod__add-price">
+              <p>{el.price}₽</p>
+            </div>
+          </div>
+        )
+      ) : (
+        // Если напитки
+        <ItemDrink
+          el={el}
+          curItem={curItem}
+          handleOptionChange={handleOptionChange}
+        />
+      )}
+    </div>
+  )
+}
+
+export default ItemModifiers
