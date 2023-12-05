cceef31f92807d0d381fefae994b737ced9b32de
Zaur Taysaev <z.taysaev2003@mail.ru>
2023-12-05 19:14:15 +0300

    add dicount component

--- src/components/Layaout.jsx
+++ src/components/Layaout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import Header from './Header'
-import Footer from './Footer'
+import DiscountComponent from './DiscountComponent'
 
 const Layaout = ({ children }) => {
   return (
     <React.Fragment>
       <Header />
+      <DiscountComponent />
       {children}
     </React.Fragment>
   )
--- src/pages/BurgerPage/BurgerPage.jsx
+++ src/pages/BurgerPage/BurgerPage.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import Item from '../../components/Item'
-import DiscountComponent from '../../components/DiscountComponent'
 
 const BurgerPage = ({ item }) => {
   return (
     <div className="main">
-      <DiscountComponent />
-
       {item.map((el) => (
         <Item key={el.id} item={el} />
       ))}
