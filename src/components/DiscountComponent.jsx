3c7221e8ac6980719159ab5551ab1cb7bb61be67
Zaur Taysaev <z.taysaev2003@mail.ru>
2023-12-05 17:21:51 +0300

    add dicount component

---
+++ src/components/DiscountComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DiscountComponent = () => {
+  return (
+    <div className="discount">
+      <span className="discount-text">Скидка 10%</span>
+    </div>
+  )
+}
+
+export default DiscountComponent
