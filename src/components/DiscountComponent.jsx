981ce3dd6c8ba5600eb42cdb7a4948dbf66363aa
Zaur Taysaev <z.taysaev2003@mail.ru>
2023-12-05 19:27:59 +0300

    dicount component style update

--- src/components/DiscountComponent.jsx
+++ src/components/DiscountComponent.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const DiscountComponent = () => {
   return (
     <div className="discount">
-      <span className="discount-text">Скидка 10%</span>
+      <span className="discount-text">
+        <span className="discount__text">Скидка</span> 10%
+      </span>
     </div>
   )
 }
