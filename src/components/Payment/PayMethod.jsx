0e2f7efcafb599654b45024b69f5ff2d8ce57062
Zaur Taysaev <z.taysaev2003@mail.ru>
2023-12-05 19:22:21 +0300

    PaymentMethod bug fixed

--- src/components/Payment/PayMethod.jsx
+++ src/components/Payment/PayMethod.jsx
@@ -14,38 +14,52 @@ const PayMethod = () => {
     <div className="pay">
       <p className="pay__text">Способ оплаты:</p>
       <div className="pay__types">
-        <div className="pay__type" onClick={() => handleOptionChange('cash')}>
-          <button className={`check ${payMethod === 'cash' ? 'checked' : ''}`}>
-            {payMethod === 'cash' && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24"
-                viewBox="0 -960 960 960"
-                width="24"
-              >
-                <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
-              </svg>
-            )}
-          </button>
-          <div className="pay__description">
-            <p>Наличными</p>
+        <div className="pay__type">
+          <div
+            className="pay__type__click"
+            onClick={() => handleOptionChange('cash')}
+          >
+            <button
+              className={`check ${payMethod === 'cash' ? 'checked' : ''}`}
+            >
+              {payMethod === 'cash' && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 -960 960 960"
+                  width="24"
+                >
+                  <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+                </svg>
+              )}
+            </button>
+            <div className="pay__description">
+              <p>Наличными</p>
+            </div>
           </div>
         </div>
-        <div className="pay__type" onClick={() => handleOptionChange('card')}>
-          <button className={`check ${payMethod === 'card' ? 'checked' : ''}`}>
-            {payMethod === 'card' && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24"
-                viewBox="0 -960 960 960"
-                width="24"
-              >
-                <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
-              </svg>
-            )}
-          </button>
-          <div className="pay__description">
-            <p>Переводом</p>
+        <div className="pay__type">
+          <div
+            className="pay__type__click"
+            onClick={() => handleOptionChange('card')}
+          >
+            <button
+              className={`check ${payMethod === 'card' ? 'checked' : ''}`}
+            >
+              {payMethod === 'card' && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 -960 960 960"
+                  width="24"
+                >
+                  <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+                </svg>
+              )}
+            </button>
+            <div className="pay__description">
+              <p>Переводом</p>
+            </div>
           </div>
         </div>
       </div>
