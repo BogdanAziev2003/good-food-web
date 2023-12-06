aae1c71bb7ff0d04ed526f94a1e4e1f689a0e18d
Zaur Taysaev <z.taysaev2003@mail.ru>
2023-12-06 22:02:57 +0300

    payment item snack and sause change position

--- src/components/Payment/ItemDropdownContent.jsx
+++ src/components/Payment/ItemDropdownContent.jsx
@@ -3,15 +3,6 @@ import React from 'react'
 const ItemDropdownContent = ({ additive, items, kitcut }) => {
   return (
     <>
-      {items.map((item, id) => {
-        if (item.amount !== 0) {
-          return (
-            <div key={id} className="dropdown-item">
-              {kitcut(item.title, 10)} x {item.amount}
-            </div>
-          )
-        }
-      })}
       {additive &&
         additive.map((add, id) => {
           if (add) {
@@ -22,6 +13,15 @@ const ItemDropdownContent = ({ additive, items, kitcut }) => {
             )
           }
         })}
+      {items.map((item, id) => {
+        if (item.amount !== 0) {
+          return (
+            <div key={id} className="dropdown-item">
+              {kitcut(item.title, 10)} x {item.amount}
+            </div>
+          )
+        }
+      })}
     </>
   )
 }
