877f5e6c32e29fe6d2fd2bdf8bc1211cbe5a543f
Zaur Taysaev <z.taysaev2003@mail.ru>
2024-01-03 13:52:32 +0300

    useTelegram discound price fix

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -18,7 +18,7 @@ export function useTelegram() {
   } catch (error) {}
 
   Telegram.WebApp.onEvent('mainButtonClicked', () => {
-    if (tg.MainButton.text === `Мой заказ: ${price}`) navigate('/payment')
+    if (tg.MainButton.text === `Мой заказ: ${price} ₽`) navigate('/payment')
   })
 
   if (window.location.pathname === '/') {
