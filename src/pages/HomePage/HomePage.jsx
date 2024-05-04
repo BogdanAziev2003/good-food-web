336761bd4ac79d9d1ec100db55dbdd060548f641
tays001 <z.taysaev2003@gmail.com>
2024-05-04 10:09:51 +0300

    action 2 add

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Item from '../../components/Item';
 
 const HomePage = ({ item }) => {
+  console.log(item);
   return (
     <div className="main">
       <div className="action">
         <h2 className="action__text">Акция</h2>
+        <Item item={item.find((el) => el.id === 84)} />
         <Item item={item.find((el) => el.id === 1)} />
       </div>
 
