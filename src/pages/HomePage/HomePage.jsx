87c215d0adf06154ace6fed13d4c7e7d59c4a7fc
tays001 <z.taysaev2003@gmail.com>
2024-08-18 15:21:27 +0300

    don`t dublicate kombo

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,13 +6,17 @@ const HomePage = ({ item }) => {
     <div className="main">
       <div className="action">
         <h2 className="action__text">Акция</h2>
+        <Item item={item.find((el) => el.id === 113)} />
         <Item item={item.find((el) => el.id === 84)} />
-
         <Item item={item.find((el) => el.id === 1)} />
       </div>
 
-      {item.map((el) => (
-        <Item key={el.id} item={el} />
+      {item.map((el, id) => (
+        <div key={id}>
+          {el.id !== 84 && el.id !== 113 && el.id !== 1 ? (
+            <Item item={el} />
+          ) : null}
+        </div>
       ))}
     </div>
   );
